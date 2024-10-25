82349b8c4588abd0b073df5f2b0094faa4326ea4
Tessa Murray <tmurray@bsd220.org>
2024-10-25 07:37:58 -0700

    Initial Commit

---
+++ Pig/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pig
+//
+//  Created by Tessa Murray on 10/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Tessa Murray on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
